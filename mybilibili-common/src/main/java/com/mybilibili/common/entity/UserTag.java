60b81ee523576f8e1d0eebe37ebefee82e0f5ab7
dllm <2036680567@qq.com>
2026-05-02 23:43:12 +0800

    fix: 修复 UserTag 实体类表名映射

--- mybilibili-common/src/main/java/com/mybilibili/common/entity/UserTag.java
+++ mybilibili-common/src/main/java/com/mybilibili/common/entity/UserTag.java
@@ -1,9 +1,11 @@
 package com.mybilibili.common.entity;
 
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 import java.util.Date;
 
 @Data
+@TableName("user_tags")
 public class UserTag {
     private Integer id;
     private Integer userId;
