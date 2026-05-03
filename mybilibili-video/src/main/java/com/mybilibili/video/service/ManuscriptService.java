0b741a56ad46ba84978448b5990285c49a8f1ab6
dllm <2036680567@qq.com>
2026-05-03 22:50:06 +0800

    feat: 添加稿件评论数更新功能（数据库+Feign调用）

--- mybilibili-video/src/main/java/com/mybilibili/video/service/ManuscriptService.java
+++ mybilibili-video/src/main/java/com/mybilibili/video/service/ManuscriptService.java
@@ -50,4 +50,7 @@ public interface ManuscriptService {
     Map<String, Object> getVideoSourceUrl(Integer videoId);
     Video getVideoById(Integer videoId);
     void incrementViewCount(Integer manuscriptId);
+    void updateCommentCount(Integer manuscriptId, Integer count);
+    void incrementCommentCount(Integer manuscriptId);
+    void decrementCommentCount(Integer manuscriptId);
 }
